8a7c141608eb85db11f5746f83195ab7198b762a
Xakep1993 <xaker-14@hotmail.com>
2016-11-14 21:47:27 +0300

    initial Android N kernel

---
+++ .missing-syscalls.d
@@ -0,0 +1,10 @@
+-:  /home/nikolay/aosp/Android7A7000/include/linux/kconfig.h \
+ include/generated/autoconf.h arch/arm64/include/generated/asm/types.h \
+ /home/nikolay/aosp/Android7A7000/include/uapi/asm-generic/types.h \
+ include/asm-generic/int-ll64.h include/uapi/asm-generic/int-ll64.h \
+ /home/nikolay/aosp/Android7A7000/arch/arm64/include/uapi/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h include/uapi/asm-generic/bitsperlong.h \
+ /home/nikolay/aosp/Android7A7000/arch/arm64/include/asm/unistd.h \
+ /home/nikolay/aosp/Android7A7000/arch/arm64/include/uapi/asm/unistd.h \
+ include/asm-generic/unistd.h include/uapi/asm-generic/unistd.h \
+ include/linux/export.h
